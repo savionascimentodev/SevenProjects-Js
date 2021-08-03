d7cf5a808f1397ce7a2b9309a99eedaaba92f121
Savio Nascimento <savionascimentodev@gmail.com>
2021-08-03 08:11:38 -0400

    ✨ project 2

--- relogios/script.js
+++ relogios/script.js
@@ -9,7 +9,20 @@ function updateClock() {
   let minute = now.getMinutes()
   let second = now.getSeconds()
 
-  digitalElement.innerHTML = `${hour}:${minute}:${second}`
+  digitalElement.innerHTML = `${fixZero(hour)}:${fixZero(minute)}:${fixZero(second)}` // Relógio Digital
+
+  let secondsDeg = ((360 / 60) * second) - 90
+  let minutesDeg = ((360 / 60) * minute) - 90
+  let hoursDeg = ((360 / 12) * hour) - 90 // Calculo de ângulo de cada um 
+
+  secondsElement.style.transform = `rotate(${secondsDeg}deg)`
+  minutesElement.style.transform = `rotate(${minutesDeg}deg)`
+  hoursElement.style.transform = `rotate(${hoursDeg}deg)` // Relógio Analógico
 }
 
+function fixZero(time) {
+  return time < 10 ? `0${time}` : time
+}// Adiciona um zero a frente dos números para deixar uma estetica mais bonita e limpa
+
 setInterval(updateClock, 1000)
+updateClock()
